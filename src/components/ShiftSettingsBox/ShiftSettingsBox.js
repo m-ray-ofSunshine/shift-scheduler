4c4bf07aca1310141f7394d78069992afb44e592
Matt Ray <sleepingbag5927@gmail.com>
2022-05-22 22:27:05 -0500

    Added styling

--- src/components/ShiftSettingsBox/ShiftSettingsBox.js
+++ src/components/ShiftSettingsBox/ShiftSettingsBox.js
@@ -1,17 +1,21 @@
 function ShiftSettingsBox() {
   return (
-    <div>
-      <h3>Shift Settings Box</h3>
-      <form>
+    <form className="form-wrapper">
+      <h3 className="sub-header">Shift Settings Box</h3>
+      <div>
         <label>Date of shift</label>
         <input type="date"></input>
+      </div>
+      <div>
         <label>Start time</label>
         <input type="time"></input>
+      </div>
+      <div>
         <label>End time</label>
         <input type="time"></input>
-        <button type="submit">Create</button>
-      </form>
-    </div>
+      </div>
+      <button type="submit">Create</button>
+    </form>
   );
 }
 
--- src/components/VolunteerSettingsBox/VolunteerSettingsBox.js
+++ src/components/VolunteerSettingsBox/VolunteerSettingsBox.js
@@ -1,20 +1,24 @@
 function VolunteerSettingsBox() {
   return (
-    <div>
-      <h3>Volunteer Settings Box</h3>
-      <form>
+    <form className="form-wrapper">
+      <h3 className="sub-header">Volunteer Settings Box</h3>
+      <div className="fieldset-wrapper">
         <label>Name</label>
         <input type="text"></input>
+      </div>
+      <div>
         <label>Email</label>
         <input type="email"></input>
+      </div>
+      <div>
         <label>Availability</label>
         <select name="shifts">
           <option value="AM">AM</option>
           <option value="PM">PM</option>
         </select>
-        <button type="submit">Create</button>
-      </form>
-    </div>
+      </div>
+      <button type="submit">Create</button>
+    </form>
   );
 }
 
